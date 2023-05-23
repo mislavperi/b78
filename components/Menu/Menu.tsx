9bcb96eeb79d0d64dd7c2c276c18768433fb63a5
Mislav Peric <mislav.peric@happening.xyz>
2023-05-23 10:04:27 +0200

    fix: removed errors

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -4,18 +4,24 @@ export function Menu() {
   return (
     <BackgroundImage src="/assets/b3darken.webp" h="100vh">
       <Flex align="center" justify="center" direction="column" h="100vh">
-          <Text align="center!important" maw={500} size={20} p={5} m={5}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsum eum inventore qui
-            magnam ea quasi? Reiciendis iure, eum quam sed vero maxime, doloremque quas totam quidem
-            ullam veritatis repellendus.
-          </Text>
-          <Button p={5} m={5}>Download our menu</Button>
-          <Text align="center!important" maw={500} size={20} p={5} m={5}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque saepe officiis,
-            dolor earum laborum, explicabo laboriosam molestias soluta magni cum, eaque fugiat fuga
-            asperiores voluptates nihil commodi voluptatibus alias.
-          </Text>
-          <Button p={5} m={5}>Download our wine card</Button>
+        {/* @ts-ignore */}
+        <Text align="center!important" maw={500} size={20} p={5} m={5}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus ipsum eum inventore qui
+          magnam ea quasi? Reiciendis iure, eum quam sed vero maxime, doloremque quas totam quidem
+          ullam veritatis repellendus.
+        </Text>
+        <Button p={5} m={5}>
+          Download our menu
+        </Button>
+        {/* @ts-ignore */}
+        <Text align="center!important" maw={500} size={20} p={5} m={5}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cumque saepe officiis,
+          dolor earum laborum, explicabo laboriosam molestias soluta magni cum, eaque fugiat fuga
+          asperiores voluptates nihil commodi voluptatibus alias.
+        </Text>
+        <Button p={5} m={5}>
+          Download our wine card
+        </Button>
       </Flex>
     </BackgroundImage>
   );
