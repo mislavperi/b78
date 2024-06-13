343d6a467292731ea38f2b402b79263ea8543b3c
Mislav Perić <mi.peric@reply.de>
2024-06-13 10:14:06 +0200

    update menus, flow

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -8,12 +8,12 @@ export function Menu() {
         <Text align="center!important" maw={500} size="calc(16px + 0.390625vw)" p={5} m={5}>
           Discover a modern twist on traditional Dalmatian cuisine at our restaurant. Our menu features fresh ingredients, including herbs from our garden. Delight in Adriatic Sea seafood or savor locally sourced sheep meat from Brač. Join us for a culinary adventure celebrating Dalmatia's essence in every bite.
         </Text>
-        <a href="https://drive.google.com/file/d/1bD3XsyEvH0favahGyrJUKln87DfahgZO/view?usp=sharing" target="_blank">
+        <a href="https://drive.google.com/file/d/1BGfDQ814HP_r5zxgbQTn9fZGb7cZZi0J/view?usp=sharing" target="_blank">
           <Button p={5} m={5}>
             View our menu in English
           </Button>
         </a>
-        <a href="https://drive.google.com/file/d/1VlW7TlSgMXDZ6priVaCYIjIxtOxAGhxm/view?usp=sharing" target="_blank">
+        <a href="https://drive.google.com/file/d/1Cxj-TmrZ8y6W8-PFeKXp3cM-h8tcZdRc/view?usp=sharing" target="_blank">
           <Button p={5} m={5}>
             View our menu in Croatian
           </Button>
@@ -22,9 +22,14 @@ export function Menu() {
         <Text align="center!important" maw={500} size="calc(16px + 0.390625vw)" p={5} m={5}>
           Explore the finest Croatian wines on our curated list. Discover flavors from Istria, Slavonia, Central Croatia, and Dalmatia. Indulge in aromatic whites, elegant reds, and refreshing rosés. Our knowledgeable staff will help you choose the perfect wine to enhance your dining experience. Cheers to Croatia's remarkable winemaking!
         </Text>
-        <a href="https://teal-elysha-39.tiiny.site/" target="_blank">
+        <a href="https://drive.google.com/file/d/1JZW_PtDR-bNDkT6T1FRPKN1oqYwXpL2o/view?usp=sharing" target="_blank">
           <Button p={5} m={5}>
             View our wine card
+          </Button>
+        </a>
+        <a href="https://drive.google.com/file/d/1vwcyXGnleIBl8NaJ3VAIcqe7pAzaee5m/view?usp=sharing" target="_blank">
+          <Button p={5} m={5}>
+            View our drinks menu
           </Button>
         </a>
 
