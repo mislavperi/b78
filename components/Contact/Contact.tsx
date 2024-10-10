e417c5e0fb41a2713bf4d71a369adb5e163c84a5
Mislav Perić <mi.peric@reply.de>
2024-10-10 08:14:31 +0200

    added closed for the season

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -64,7 +64,7 @@ export function Contact() {
           </Flex>
         </Container>
         <Text>Ul. Radića Frane 14</Text>
-        <Text>Open everyday from 12h to 24h</Text>
+        <Text>Closed for the season. Thank you!</Text>
       </Flex>
     </BackgroundImage>
   );
